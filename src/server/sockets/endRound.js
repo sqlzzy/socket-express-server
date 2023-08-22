22074720d20f28ac72520fc41ebc6f8aac3228a1
Sergey Osipov <osipovsergei97@gmail.com>
2023-08-22 23:48:53 +0300

    Return preview version endRound.js

--- src/server/sockets/endRound.js
+++ src/server/sockets/endRound.js
@@ -1,13 +1,16 @@
 function endRound(socket, rooms, io, roomId) {
     const room = rooms.get(roomId);
-    const indexLastPlayer = room.players.length - 1;
-    const firstPlayer = room.players[0];
-    const nextPlayer = room.players[indexCurrentHost + 1];
 
     if (room.host === socket.id && room.players.length > 1) {
         const currentHost = room.players.find(player => player.idPlayer === room.host);
         const indexCurrentHost = room.players.findIndex(player => player.host === 1);
-        const nextHost = indexCurrentHost === indexLastPlayer ? firstPlayer : nextPlayer;
+        let nextHost;
+
+        if (indexCurrentHost === room.players.length - 1) {
+            nextHost = room.players[0];
+        } else {
+            nextHost = room.players[indexCurrentHost + 1];
+        }
 
         const updatedPlayers = room.players.map(player => {
             if (player.host === 1) {
@@ -18,9 +21,9 @@ function endRound(socket, rooms, io, roomId) {
         });
 
         if (nextHost) {
+            room.host = nextHost.idPlayer;
             currentHost.host = 0;
             nextHost.host = 1;
-            room.host = nextHost.idPlayer;
 
             io.to(nextHost.idPlayer).emit('becomeHost');
             io.to(currentHost.idPlayer).emit('endBecomeHost');
