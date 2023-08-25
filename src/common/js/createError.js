c119a6c14ab88c88cb08771f0addbc17cfe2f60d
Sergey Osipov <osipovsergei97@gmail.com>
2023-08-25 23:14:41 +0300

    Add createError.js

---
+++ src/common/js/createError.js
@@ -0,0 +1,11 @@
+function createError(text) {
+    const spanError = document.createElement('span');
+    spanError.textContent = text;
+    spanError.style.color = 'red';
+    spanError.style.marginLeft = '12px';
+    spanError.setAttribute('id', 'error-message');
+
+    return spanError;
+}
+
+export default createError;
